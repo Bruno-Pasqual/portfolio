07131f2780a096706fc22355bf3bce400d7e23ac
Bruno Pasqual <brunoh.pasqual0@gmail.com>
2023-06-02 11:04:53 -0300

    teste

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-window.addEventListener('scroll', (e) => {
+/* window.addEventListener('scroll', (e) => {
   console.log(window.pageYOffset);
+});
+ */
+
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('ativo');
 });
